83fcff0b97c2f568c716ac619c72018d6902fbe2
Michelle Reyes <greyes.loza@gmail.com>
2017-11-05 23:22:38 -0600

    grafica edad

--- src/componentes/ChartEdad.js
+++ src/componentes/ChartEdad.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
+import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+
+import './ChartEdad.css'
+
+const data = [
+      {name: '0-5', Hombres: 4, Mujeres: 2, amt: 2},
+      {name: '6-10', Hombres: 3, Mujeres: 1, amt: 2},
+      {name: '11-15', Hombres: 5, Mujeres: 9, amt: 2},
+      {name: '16-20', Hombres: 2, Mujeres: 3, amt: 2},
+      {name: '21-25', Hombres: 8, Mujeres: 5, amt: 2},
+      {name: '26-30', Hombres: 2, Mujeres: 3, amt: 2},
+      {name: '31-35', Hombres: 3, Mujeres: 4, amt: 2},
+];
+
 class ChartEdad extends Component{
     render(){
         return(
-            <div>
-                <h1>Chart edad</h1>
+            <div className="chart">
+                <BarChart width={1000} height={500} data={data}
+                    margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                    <XAxis dataKey="name"/>
+                    <YAxis/>
+                    <CartesianGrid strokeDasharray="3 3"/>
+                    <Tooltip/>
+                    <Legend />
+                    <Bar dataKey="Hombres" fill="#4571DA" />
+                    <Bar dataKey="Mujeres" fill="#DB5A6E" />
+                </BarChart>
             </div>
         )
     }
