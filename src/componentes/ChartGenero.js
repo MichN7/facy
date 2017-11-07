2691f0e90d6f3759b9297822a11a0b2a7257f3de
Michelle Reyes <greyes.loza@gmail.com>
2017-11-07 16:26:02 -0600

    diseno graficas completo

--- src/componentes/ChartGenero.js
+++ src/componentes/ChartGenero.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import {Doughnut} from 'react-chartjs-2';
+
+const data = {
+    labels:[
+        'Hombres',
+        'Mujeres'
+    ],
+    datasets:[{
+        data:[30,20],
+        backgroundColor:[
+            '#4571DA',
+            '#DB5A6E'
+        ],
+        hoverBackgroundColor:[
+            '#4571DA',
+            '#DB5A6E'
+        ],
+    }]
+}
+
+const styles = {
+    width:'82%',
+    padding:'3%',
+    marginLeft: '9%'
+   }
 
 class ChartGenero extends Component{
     render(){
         return(
-            <div>
-                
+            <div style={styles}>
+                <Doughnut data={data} />
             </div>
         )
     }
